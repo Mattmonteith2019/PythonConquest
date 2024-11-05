ffb3a05628456d41767c221ffd240c9be7a3af90
matt monteith <matthew.monteith@gmail.com>
2024-11-04 23:29:54 -0500

    run, then see comments in lines 117-121

--- pythonconquest.py
+++ pythonconquest.py
@@ -1,155 +1,130 @@
 import random
 
-# My original function for generating room
-# def room_list(sequence):
-#    return random.choice(sequence)
-# options = ["Dungeon", "Treasury", "Library"]
-# room = room_list(options)
+print(
+        r"______      _   _                 _____                                   _   "
+    )
+print(
+        r"| ___ \    | | | |               /  __ \                                 | |  "
+    )
+print(
+        r"| |_/ /   _| |_| |__   ___  _ __ | /  \/ ___  _ __   __ _ _   _  ___  ___| |_ "
+    )
+print(
+        r"|  __/ | | | __| '_ \ / _ \| '_ \| |    / _ \| '_ \ / _` | | | |/ _ \/ __| __|"
+    )
+print(
+        r"| |  | |_| | |_| | | | (_) | | | | \__/\ (_) | | | | (_| | |_| |  __/\__ \ |_ "
+    )
+print(
+        r"\_|   \__, |\__|_| |_|\___/|_| |_|\____/\___/|_| |_|\__, |\__,_|\___||___/\__|"
+    )
+print(
+        r"       __/ |                                           | |                    "
+    )
+print(
+        r"      |___/                                            |_|                    "
+    )
+print()
 
-
-# Brandon's function for generate room...doesn't allow me to use the return value in HUD
-### Brandons Comment to that you need to add an input in the return to do that... but im gonna remove the hud for now...
-def generate_room():
-    room_type = ["Dungeon", "Treasury", "Library"]
-    return random.choice(room_type)
-
-
-narrator = "NARRATOR: "
-
-
-# player class
-# so what i changed here is removed the health variable and hardcoded a value of 100... that way when you create the player he has 100 health.
+# PLAYER CLASS
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, health):
         self.name = name
-        self.health = 100
-
-    # here is the player attack. It will eventually take in the enemy along with self, but for now its just doing the damage and printing.
-    def attack(self):
+        self.health = health
+        self.damage = 0
+        
+    def attack(self, enemy):
         damage = random.randint(10, 20)
-        # we will put enemy damage here when its dime.
+        enemy1.health -= damage
+        # we will put enemy damage here when its time.
         print(f"{self.name} attacks -- for {damage} damage!")
 
+player_name = input("Enter your Player name: ")
+player = Player(player_name, 0)
+player.health = 100
 
-# get rid of this p1 = Player... the init looks good.
-# p1 = Player("BRANDON", 100)
 
 
-# enemy class
+# ENEMY CLASS
 class Enemy:
     def __init__(self, name, health):
         self.name = name
         self.health = health
 
-    def attack(self):
-        damage = random.randint(1, 10)
-        print(f"{self.name} attacks for {damage} damage!")
+    def attack(self, player):
+        damage = random.randint(1, 20)
+        player.health -= damage
+        print(f"{self.name} attacks {player_name} with {weapon} for {damage} damage!")
         return damage
 
 
-# so this is the spawn enemy function. I ttakes the enemies in a list and randomly chooses one to spawn.
+# RANDOM ENEMY SPAWN
 def spawn_enemy():
-    enemies = [("Ogre", 100), ("Troll", 60), ("Goblin", 50), ("Dragon", 200), ("Witch", 75)]
+    enemies = [("Ogre", 100), 
+               ("Troll", 60), 
+               ("Goblin", 50), 
+               ("Dragon", 200), 
+               ("Witch", 75)
+               ]
     name, health = random.choice(enemies) #it takes both values. the name and health.
     return Enemy(name, health) # it returns the enemy object with the name and health.
+enemy1 = spawn_enemy() 
 
 
-# so all you have to do is this to crate it...
-enemy1 = spawn_enemy() # so doing this CREATES the enemy object with the name and health. Because remember running the functions RETURNS the object.
-print(enemy1.name, enemy1.health) #this should be random every time you run it.
+#RANDOM ROOM SPAWN
+def generate_room():
+    room_type = ["Dungeon", 
+                 "Treasury", 
+                 "Library"
+                 ]
+    return random.choice(room_type)
+room = generate_room()
 
-#So to do an attack....
-enemy1.attack() #this will print out the attack damage.
-#now when your ready to do actual damage.... you are going to want to pass the player object in and do a player.health -= damage inside the attack function...
-# I think you should try that.
-
-# I added inputs into your  hud and then fixed the inputs in the
-def display_hud(player, room):
-    print(r"_______________________________________________________________________")
-    print(r"|  PLAYER HEALTH  |         ROOM         |   ENEMY   |  ENEMY HEALTH   |")
-    print(
-        r"|______", player.health, "______|___", room, "__|__"
-    )  # name,"_|______")#Enemy.health,"______|")im leaving this commented out.. because we dont have this input yet.
-    print()
-    print("Select from the following options: ")
-    print(" 1. Attack")
-    print(" 2. Flee")
-    print(" 3. Exit game")
-    print()
+#RANDOM ENEMY WEAPON
+def enemy_weapon():
+    weapons = ["sword",
+               "club",
+               "dagger"
+               ]
+    return random.choice(weapons)
+weapon = enemy_weapon()
 
 
-# I added the inputs into the function, then i fixed the inputs in the statements.
-def story_intro(player, room):
-    print(
-        r"______      _   _                 _____                                   _   "
-    )
-    print(
-        r"| ___ \    | | | |               /  __ \                                 | |  "
-    )
-    print(
-        r"| |_/ /   _| |_| |__   ___  _ __ | /  \/ ___  _ __   __ _ _   _  ___  ___| |_ "
-    )
-    print(
-        r"|  __/ | | | __| '_ \ / _ \| '_ \| |    / _ \| '_ \ / _` | | | |/ _ \/ __| __|"
-    )
-    print(
-        r"| |  | |_| | |_| | | | (_) | | | | \__/\ (_) | | | | (_| | |_| |  __/\__ \ |_ "
-    )
-    print(
-        r"\_|   \__, |\__|_| |_|\___/|_| |_|\____/\___/|_| |_|\__, |\__,_|\___||___/\__|"
-    )
-    print(
-        r"       __/ |                                           | |                    "
-    )
-    print(
-        r"      |___/                                            |_|                    "
-    )
-    print()
-    print(f"{narrator}...wake up...")
-    print(f"{narrator}...wake up...")
-    print(f"{narrator}Wake up", player.name, "!")
-    print(f"{player.name}: Where am I?? Who are you??")
-    print(f"{narrator}You have awaken inside the", room, "...")
-    print(f"{player.name}: Why am I here? How did I get here? ...and WHO ARE YOU?!")
-    print(
-        f"{narrator}My name is Master Scrum.  The Coding Village is under seige by the sinister Lord Python and his evil horde of creatures..."
-    )
-    print(
-        f"{narrator}There's no time to waste! Stand up and draw your sword and prepare to defend Coding Village!"
-    )
-    # print(f"{narrator}Watch out! There's a",enemy,"nearby!")
-    print()
+#PRINT STATEMENTS TO CHECK THINGS ARE WORKING PROPERLY
+print(f"{player.name} is your player name!")
+print()
+print(f"Your current health = {player.health}")
+print()
+print(f"A {enemy1.name} with {enemy1.health} health has spawned randomly! They're wielding a {weapon}.")
+print()
+player.attack(enemy1)
+print()
+print(f"{enemy1.name} health has diminished to {enemy1.health}")
+print()
+enemy1.attack(player)
+print()
+print(f"{player.name} health has diminished to {player.health}!")
+print()
+player.attack(enemy1)
+print()
+print(f"{enemy1.name} health has diminished to {enemy1.health}")
+print()
+enemy1.attack(player)
+print()
+print(f"{player.name} health has diminished to {player.health}!")
+print()
+
+#NEXT STEP IS TO CREATE A MENU FOR THE PLAYER TO DECIDE WHETHER TO ATTACK, HEAL, EXIT. 
+#SIMILAR TO VURTUAL PRINTER..WON'T BE ABLE TO HEAL BEYOND 100.
+#AFTER EACH CHOICE TO EITHER ATTACK OR HEAL, PLAYER MUST ENTER A CHOICE TO KEEP ATTACKING OR HEAL OR EXIT
+#STILL NEED TO INTRODUCE THE WHILE PLAYER IS ALIVE
+#ADD YOUR COMMENTS
 
 
-# I removed Flee for now....
+# FUNCTION TO RUN PROGRAM
+#def run():
 
-# ok... so i have no more run function, but i want to show you how this would work.
-
-# first we create a player....
-player_name = input("Enter your player's name: ")
-player = Player(player_name)
-
-# now we can pass this into the story intro and display hud functions
-story_intro(player, generate_room())
-
-
-""" # Function to run program
-def run():
-    story_intro()
-    while p1.health > 0:
-        display_hud()
-        choice = input("Choose wisely (1-3): ")
-        print()
-        if choice == "1":
-            player_attack()
-        elif choice == "2":
-            player_flee()
-        elif choice == "3":
-            print(f"{narrator}YOU COWARD! You miserable worm! The Coding Village was counting on you to fight!")
-            break
-        else:
-            print("Invalid entry, please try again.")
 
 # This runs program
-if __name__ == "__main__":
-    run() """
+#if __name__ == "__main__":
+    #run()
